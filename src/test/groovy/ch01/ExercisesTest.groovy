c9c3057c701e59399e16f4d6ba76ab3fde529894
Daniel Silva <daniel@silvanolte.com>
2017-07-08 23:10:29 +0200

    Updated comment in test class for Chapter 1, Exercise 1.

--- src/test/groovy/ch01/ExercisesTest.groovy
+++ src/test/groovy/ch01/ExercisesTest.groovy
@@ -4,8 +4,11 @@ import org.junit.Test
 
 class ExercisesTest {
 
+  // Implement, as best as you can, the identity function in your favorite
+  // language (or the second favorite, if your favorite language happens
+  // to be Haskell).
   @Test
-  void identityFunctionTest() {
+  void identityTest() {
     (1..100).each {
       def x = new Object()
       assert x == Exercises.identity(x)
